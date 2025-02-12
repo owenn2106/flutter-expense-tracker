2761ccc3b4871e202a8cc2759781e3413fe88bf9
Owenn Gimli <75826315+owenn2106@users.noreply.github.com>
2025-02-12 18:58:11 +0700

    Render datepicker

--- lib/widgets/add_expense_form.dart
+++ lib/widgets/add_expense_form.dart
@@ -13,6 +13,17 @@ class _AddExpenseFormState extends State<AddExpenseForm> {
   final _titleController = TextEditingController();
   final _amountController = TextEditingController();
 
+  void _presentDatePicker() {
+    final now = DateTime.now();
+    final firstDate = DateTime(now.year - 1, now.month, now.day);
+    showDatePicker(
+      context: context,
+      initialDate: now,
+      firstDate: firstDate,
+      lastDate: now,
+    );
+  }
+
   // We should always dispose controller - because it's a resource that needs to be cleaned up
   @override
   void dispose() {
@@ -32,11 +43,31 @@ class _AddExpenseFormState extends State<AddExpenseForm> {
             maxLength: 50,
           ),
           const SizedBox(height: 10),
-          TextField(
-            controller: _amountController,
-            decoration:
-                const InputDecoration(labelText: "Amount", prefixText: "\$"),
-            keyboardType: TextInputType.number,
+          Row(
+            children: [
+              Expanded(
+                child: TextField(
+                  controller: _amountController,
+                  decoration: const InputDecoration(
+                      labelText: "Amount", prefixText: "\$"),
+                  keyboardType: TextInputType.number,
+                ),
+              ),
+              const SizedBox(width: 16),
+              Expanded(
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    const Text("Selected date"),
+                    IconButton(
+                      icon: const Icon(Icons.calendar_month),
+                      onPressed: _presentDatePicker,
+                    ),
+                  ],
+                ),
+              ),
+            ],
           ),
           const SizedBox(height: 10),
           const SizedBox(height: 20),
