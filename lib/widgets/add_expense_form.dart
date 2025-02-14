9e5628dc92c9c81800b80026aba6be3000c07f37
Owenn Gimli <75826315+owenn2106@users.noreply.github.com>
2025-02-14 18:45:05 +0700

    Add category dropdown button

--- lib/widgets/add_expense_form.dart
+++ lib/widgets/add_expense_form.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import "package:expense_tracker/models/expense.dart";
 
 class AddExpenseForm extends StatefulWidget {
   const AddExpenseForm({super.key});
@@ -12,16 +13,21 @@ class AddExpenseForm extends StatefulWidget {
 class _AddExpenseFormState extends State<AddExpenseForm> {
   final _titleController = TextEditingController();
   final _amountController = TextEditingController();
+  DateTime? _selectedDate;
+  Category? _selectedCategory = Category.leisure;
 
-  void _presentDatePicker() {
+  void _presentDatePicker() async {
     final now = DateTime.now();
     final firstDate = DateTime(now.year - 1, now.month, now.day);
-    showDatePicker(
+    final pickedDate = await showDatePicker(
       context: context,
       initialDate: now,
       firstDate: firstDate,
       lastDate: now,
     );
+    setState(() {
+      _selectedDate = pickedDate;
+    });
   }
 
   // We should always dispose controller - because it's a resource that needs to be cleaned up
@@ -49,7 +55,9 @@ class _AddExpenseFormState extends State<AddExpenseForm> {
                 child: TextField(
                   controller: _amountController,
                   decoration: const InputDecoration(
-                      labelText: "Amount", prefixText: "\$"),
+                    labelText: "Amount",
+                    prefixText: "\$",
+                  ),
                   keyboardType: TextInputType.number,
                 ),
               ),
@@ -59,7 +67,11 @@ class _AddExpenseFormState extends State<AddExpenseForm> {
                   mainAxisAlignment: MainAxisAlignment.end,
                   crossAxisAlignment: CrossAxisAlignment.center,
                   children: [
-                    const Text("Selected date"),
+                    Text(
+                      _selectedDate == null
+                          ? "Selected date"
+                          : dateFormatter.format(_selectedDate!),
+                    ),
                     IconButton(
                       icon: const Icon(Icons.calendar_month),
                       onPressed: _presentDatePicker,
@@ -73,6 +85,26 @@ class _AddExpenseFormState extends State<AddExpenseForm> {
           Row(
             mainAxisAlignment: MainAxisAlignment.end,
             children: [
+              DropdownButton(
+                value: _selectedCategory,
+                items: Category.values
+                    .map(
+                      (value) => DropdownMenuItem(
+                        value: value,
+                        child: Text(
+                          value.name.toUpperCase(),
+                        ),
+                      ),
+                    )
+                    .toList(),
+                onChanged: (value) {
+                  if (value == null) return;
+                  setState(() {
+                    _selectedCategory = value;
+                  });
+                },
+              ),
+              const Spacer(),
               TextButton(
                 onPressed: () {
                   Navigator.pop(context);
